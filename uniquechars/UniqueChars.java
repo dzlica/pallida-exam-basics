9b33138637a6c76617c0aa36b435933005feea2b
Magdalena Dzindzisz <dzlica@gmail.com>
2017-10-06 13:18:53 +0200

    the original

--- uniquechars/UniqueChars.java
+++ uniquechars/UniqueChars.java
@@ -1,5 +1,7 @@
 public class UniqueChars {
     public static void main(String[] args) {
+
+
         // Create a function called `uniqueCharacters` that takes a string as parameter
         // and returns a list with the unique letters of the given string
         // Create basic unit tests for it with at least 3 different test cases
@@ -7,4 +9,5 @@ public class UniqueChars {
         // Should print out:
         // ["n", "g", "r", "m"]
     }
+
 }
