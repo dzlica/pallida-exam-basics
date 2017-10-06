67aa69adba8f7029c30f72f4028a2c7e648472ac
Magdalena Dzindzisz <dzlica@gmail.com>
2017-10-06 13:08:52 +0200

    Test still fails, but time up

--- Test/UniqueCharsTest.java
+++ Test/UniqueCharsTest.java
@@ -6,14 +6,28 @@ import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class UniqueCharsTest {
-    //ArrayList<Character> testOutput = new ArrayList<>();
+    ArrayList<Character> testOutput = new ArrayList<>();
+    ArrayList<Character> testOutput2 = new ArrayList<>();
+    ArrayList<Character> testOutput3 = new ArrayList<>();
+    testOutput.add("n");
+
     String testWord1 = "anagram";
-    String testWord2 = "somethinglongerthanthefirstone";
-    String testWord3 = "aaa";
+    String testWord2 = "longerthanthefirstone";
+    String testWord3 = "aaaaaaaaargh";
 
     @Test
     public void testUniqueCharacters() {
-        assertEquals(("n", "g", "r", "m"), testWord1);
+        assertEquals(testOutput, testWord1);
+    }
+
+    @Test
+    public void testUniqueCharacters2() {
+
+    }
+
+    @Test
+    public void testUniqueCharacters3() {
+
     }
 
 }
